2e178ea76041827581249c13a08620ed4a922df8
Alonso Ortiz <alonsoortiz73@gmail.com>
2023-01-12 13:25:40 -0800

    added "api/date" endpoint

--- index.js
+++ index.js
@@ -5,6 +5,19 @@
 var express = require('express');
 var app = express();
 
+var livereload = require('livereload')
+var connectLiveReload = require('connect-livereload')
+
+const liveReloadServer = livereload.createServer()
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/")
+  }, 100)
+})
+
+app.use(connectLiveReload())
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,9 +37,41 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// ? makes the parameter optional
+app.get("/api/:date?", (req, res) => {
+  
+  // the {} around the variable name is used to as a destructor
+  /*
+    Ex:
+        let arr = [1, 2, 3]
 
+        var [one, two, three] = arr
+  */
+
+  var date = req.params.date
+
+  if( !(date) ) {
+    date = new Date()
+
+  } else if ( /^\d+$/.test(date) ){
+
+    date = new Date(date * 1000)
+
+  } else {
+    date = new Date(date)
+  }
+
+  console.log(date)
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
+
+var port = process.env.PORT || 5050
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
